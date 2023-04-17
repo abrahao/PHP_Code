e7b6c83137324314297763a3bc3cc3ba3cba3e3f
Abrahao Eneias <abrahao.eneias@dce.ufpb.br>
2023-04-17 13:15:18 -0300

    Funcao listar

--- Mini_CRUD/listar_usuarios.php
+++ Mini_CRUD/listar_usuarios.php
@@ -3,8 +3,28 @@
 <?php
 $sql = "SELECT * FROM usuarios";
 $res = $conn->query($sql);
-$qtd = $res->num_rows;
+$qtd = $res->rowCount();
 
-
-
+if ($qtd > 0) {
 ?>
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">nome</th>
+      <th scope="col">E-mail</th>
+      <th scope="col">Data de Nascimento</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php while ($row = $res->fetchObject()) { ?>
+    <tr>
+      <th scope="row"><?= $row->id ?></th>
+      <td><?= $row->nome ?></td>
+      <td><?= $row->email?></td>
+      <td><?= $row->nascimento?></td>
+    </tr>
+  </tbody>
+  <?php } 
+}?>
+</table>
